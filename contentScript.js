2f28b8eeb64ebdf0d29e2f006d41349829ec9422
kwojcicki <kwojcicki@sympatico.ca>
2019-11-13 22:27:05 -0600

    Progress

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,11 @@
 const key = new JSEncrypt({ default_key_size: 2056 });
 
+$(document).keypress(function(e) {
+  console.log(e);
+  console.log(e.target);
+  console.log("asd: " + e.which);
+});
+
 console.log(key.getPrivateKey());
 console.log(key.getPublicKey());
 
@@ -62,6 +68,108 @@ setTimeout(function() {
   $("._3oh-._58nk").each(function(index, node) {
     const href = location.href;
     $(node).text(decrypt($(node).text()));
+  });
+
+  // should only be 1
+  $("div._5rpb").each(function(index, node) {
+    console.log(
+      $(node)
+        .children()
+        .first()
+    );
+    $(node)
+      .children()
+      .first()
+      .keypress(function(e) {
+        var key = e.which;
+        console.log("blah: " + key);
+        if (key == 13) {
+          // the enter key code
+          //$('input[name = butAssignProd]').click();
+          //return false;
+          //$(node).children().first().text = "testing ooga booga\n"
+        }
+      });
+  });
+
+  function asd(k) {
+    var oEvent = new KeyboardEvent('keypress',{'key':'a'});
+
+    // Chromium Hack
+    Object.defineProperty(oEvent, 'keyCode', {
+                get : function() {
+                    return this.keyCodeVal;
+                }
+    });     
+    Object.defineProperty(oEvent, 'which', {
+                get : function() {
+                    return this.keyCodeVal;
+                }
+    });     
+
+    oEvent.keyCodeVal = k;
+
+    if (oEvent.keyCode !== k) {
+        alert("keyCode mismatch " + oEvent.keyCode + "(" + oEvent.which + ")");
+    }
+
+    console.log(oEvent)
+    document.querySelector("._5rpb > div").dispatchEvent(oEvent);
+}
+  setTimeout(function() {
+    console.log("sending");
+    var e = $.Event("keypress", { which: 100 });
+    $("div._5rpb > div").trigger(e);
+    e = $.Event("keypress", { which: 13 });
+    $("div._5rpb > div").trigger(e);
+    document.querySelector("._5rpb > div").dispatchEvent(new KeyboardEvent('keypress',{'key':'a'}));
+    asd(100)
+  }, 5000);
+  
+
+  $("div._5rpb > div")
+  const clonet = $("div._5rpb > div").clone()
+  
+  $("div._5rpb").append(clonet)
+  
+  // recreate input field
+  // element.dispatchEvent(new InputEvent('textInput', {data: keyChar, bubbles: true}));
+  // click send
+
+  console.log($("._5rpb > div > div > div > div > span > span"));
+  //document.querySelector("._5rpb > div > div > div > div > span > span").onkeypress  = function(){
+  //    console.log("here?")
+  //  }
+
+  //  setTimeout(function(){
+  //    $("div._5rpb").children().first().text("test oga boga")
+  //}, 5000)
+
+  console.log(
+    $._data(
+      $("div._5rpb")
+        .children()
+        .first()
+        .get(),
+      "events"
+    )
+  );
+  console.log($._data($("div._1mf._1mj").get(), "events"));
+  console.log($._data(document, "events"));
+  console.log(
+    $._data(
+      $("div._5rpb")
+        .children()
+        .first(),
+      "events"
+    )
+  );
+
+  $.each($._data(document, "events"), function(i, event) {
+    console.log(i);
+    $.each(event, function(j, h) {
+      console.log("- " + h.handler);
+    });
   });
 
   // Save it using the Chrome extension storage API.
